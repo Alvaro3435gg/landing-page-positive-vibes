794c29c2e283e19a8f5ede9fd752f46e84904684
Alvaro <alvaro.a.cruz.j@gmail.com>
2023-11-08 15:47:43 -0600

    icon var fixed

--- js/main.js
+++ js/main.js
@@ -4,23 +4,18 @@ document.addEventListener('DOMContentLoaded', function () {
   const navLinks = document.querySelector('.nav-links');
   const mobileMenuLogo = document.querySelectorAll('.mobile-menu-logo');
 
-  // Agrega un controlador de eventos para hacer clic en "menu-icon"
-  menuIcon.addEventListener('click', function () {
-    // Verifica el estado actual de la propiedad "display" de "nav-links"
+    menuIcon.addEventListener('click', function () {
+
     if (navLinks.style.display === 'none' || navLinks.style.display === '') {
-      // Si está oculto, cambia a "display: flex"
       navLinks.style.display = 'flex';
     } else {
-      // Si ya está visible, ocúltalo nuevamente
       navLinks.style.display = 'none';
     }
   });
 
-  // Agrega un controlador de eventos para hacer clic en "mobile-menu-logo"
   mobileMenuLogo.forEach(function (logo) {
     logo.addEventListener('click', function () {
-      // Oculta "nav-links" al hacer clic en una imagen "mobile-menu-logo"
-      navLinks.style.display = 'none';
+            navLinks.style.display = 'none';
     });
   });
 });
